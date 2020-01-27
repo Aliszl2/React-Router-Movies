c94788fa30755581384a8262a25fbe69b22d5e1a
Alison Law <alisparklaw@gmail.com>
2020-01-27 14:02:36 +0000

    Individual films wip

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,30 @@
-import React, { useState } from 'react';
-import { Route, Switch, Link } from 'react-router-dom';
+import React from 'react';
+// , { useState }
+import { Route, Switch} from 'react-router-dom';
 import MovieList from "./Movies/MovieList";
+import Movie from "./Movies/Movie";
 
-import SavedList from './Movies/SavedList';
+// import SavedList from './Movies/SavedList';
 
 const App = () => {
-  const [savedList, setSavedList] = useState( [] );
+  // const [savedList, setSavedList] = useState( [] );
 
-  const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
-  };
+  // const addToSavedList = movie => {
+  //   setSavedList( [...savedList, movie] );
+  // };
 
   return (
     <div>
-      <SavedList list={savedList} />
+      {/* <SavedList list={savedList} /> */}
       <Switch>  
 <Route exact path = "/">
   <MovieList/>
 </Route>
-
+<Route path = "/movie/:id">
+  <Movie/>
+</Route>
       </Switch>
-      <div>Replace this Div with your Routes</div>
+      {/* <div>Replace this Div with your Routes</div> */}
     </div>
   );
 };
